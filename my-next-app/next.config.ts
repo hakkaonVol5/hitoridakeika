6390f10208e0a3e56d533f35719bc72be724cc7a
Sota Ojika <kitune25252@gmail.com>
2025-06-20 23:08:41 +0900

    fix: デプロイできるように

--- my-next-app/next.config.ts
+++ my-next-app/next.config.ts
@@ -3,6 +3,15 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'standalone',
+  trailingSlash: false,
+  images: {
+    unoptimized: true,
+  },
+  pageExtensions: ['tsx', 'ts', 'jsx', 'js'],
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
 };
 
 export default nextConfig;
